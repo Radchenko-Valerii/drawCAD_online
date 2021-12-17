95aac8331d21f650fa7e0199754df7d8615e1c94
Valerii <valeravivaz@gmail.com>
2021-12-17 02:00:38 +0200

    Add front part of app structure
    - add main components;
    - add main style-files;

---
+++ client/src/components/Canvas.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  render() {
+    return (
+      <div className="canvas">
+        <canvas>
+
+        </canvas>
+      </div>
+    );
+  }
+}
+
+export default Canvas;
