9e52cbc3c8248418121b7b69507202993664a733
Valerii <valeravivaz@gmail.com>
2021-12-22 02:14:48 +0200

    Add buttons of tools & color input

--- client/src/components/ToolsBar/index.jsx
+++ client/src/components/ToolsBar/index.jsx
@@ -4,7 +4,15 @@ import styles from './toolsbar.module.scss'
 const ToolsBar = () => {
   return (
     <div className={styles.toolsbar}>
-      <button className={styles.brush}/>
+      <button className={styles.brush} title="brush"/>
+      <button className={styles.eraser} title="eraser"/>
+      <button className={styles.line} title="line"/>
+      <button className={styles.circle} title="circle"/>
+      <button className={styles.rect} title="rectangle"/>
+      <input type="color" className={styles.palitra} title="color"/>
+      <button className={styles.undo} title="undo"/>
+      <button className={styles.redo} title="redo"/>
+      <button className={styles.save} title="save"/>
     </div>
   );
 }
