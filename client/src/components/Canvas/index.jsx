f5103e0d6f4fab16771ac496d6c748d92885a1d6
Valerii <valeravivaz@gmail.com>
2021-12-22 20:26:02 +0200

    Add canvas component main logic

--- client/src/components/Canvas/index.jsx
+++ client/src/components/Canvas/index.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import styles from "./canvas.module.scss"
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useRef } from "react";
+import canvasState from "../../store/canvasState";
+import toolState from "../../store/toolState";
+import Brush from "../../tools/Brush";
+import styles from "./canvas.module.scss";
 
+const Canvas = observer(() => {
+  const canvasRef = useRef();
 
-const Canvas = () => {
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current);
+    toolState.setTool(new Brush(canvasRef.current));
+  }, []);
+
   return (
     <div className={styles.canvas}>
-      <canvas width={840} height={594}/>
+      <canvas ref={canvasRef} width={840} height={594} />
     </div>
   );
-}
+});
 
 export default Canvas;
-
