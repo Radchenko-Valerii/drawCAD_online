277e9d4c2ed9b1eb1d618f9e97e05c66cfeffcbf
Valerii <valeravivaz@gmail.com>
2021-12-23 15:00:37 +0200

    Add experimental circle tool logic
    - not good working at left-hand side of canvas

---
+++ client/src/tools/Circle.js
@@ -0,0 +1,54 @@
+import Tool from "./Tool";
+
+export default class Circle extends Tool {
+  constructor(canvas){
+    super(canvas);
+    this.listen()
+  }
+
+  listen(){
+    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+  }
+
+  mouseUpHandler(event){
+    this.mouseDown = false;
+  }
+
+  mouseDownHandler(event){
+    this.mouseDown = true;
+    this.ctx.beginPath();
+    this.startX = event.pageX - event.target.offsetLeft;
+    this.startY = event.pageY - event.target.offsetTop;
+    this.savedCanvas = this.canvas.toDataURL();
+  }
+
+  mouseMoveHandler(event){
+    if(this.mouseDown){
+      let currentX = event.pageX - event.target.offsetLeft;
+      let currentY = event.pageY - event.target.offsetTop;
+      let width = currentX - this.startX;
+      let height = currentY - this.startY;
+      let radius = Math.max(width, height);
+      this.draw(this.startX, this.startY, radius);
+    }
+  }
+
+  draw(x, y, radius){
+    const img = new Image();
+    img.src = this.savedCanvas;
+    img.onload = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.beginPath()
+      
+      this.ctx.ellipse(x, y, radius, radius, 0, 0, 2*Math.PI);
+      // this.ctx.fill("evenodd");
+      this.ctx.stroke();
+      this.ctx.strokeStyle = "#000000";
+    } 
+    
+  }
+
+}
