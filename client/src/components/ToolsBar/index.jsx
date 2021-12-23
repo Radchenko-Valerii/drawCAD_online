b94baefb2c7a675fb18b5da455794ce4c4feb011
Valerii <valeravivaz@gmail.com>
2021-12-23 15:01:04 +0200

    Add circle button onClick function

--- client/src/components/ToolsBar/index.jsx
+++ client/src/components/ToolsBar/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import canvasState from '../../store/canvasState';
 import toolState from '../../store/toolState';
 import Brush from '../../tools/Brush';
+import Circle from '../../tools/Circle';
 import Rect from '../../tools/Rect';
 import styles from './toolsbar.module.scss'
 
@@ -11,7 +12,7 @@ const ToolsBar = () => {
       <button className={styles.brush} title="brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}/>
       <button className={styles.eraser} title="eraser"/>
       <button className={styles.line} title="line"/>
-      <button className={styles.circle} title="circle"/>
+      <button className={styles.circle} title="circle" onClick={()=> toolState.setTool(new Circle(canvasState.canvas))}/>
       <button className={styles.rect} title="rectangle" onClick={()=> toolState.setTool(new Rect(canvasState.canvas))}/>
       <input type="color" className={styles.palitra} title="color"/>
       <button className={styles.undo} title="undo"/>
