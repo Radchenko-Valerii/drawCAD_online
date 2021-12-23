d0513f4a6d4b99b59a7fc15887dde81693be0c15
Valerii <valeravivaz@gmail.com>
2021-12-23 14:30:54 +0200

    Add botton's onClick functions for rect & brush

--- client/src/components/ToolsBar/index.jsx
+++ client/src/components/ToolsBar/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import canvasState from '../../store/canvasState';
+import toolState from '../../store/toolState';
+import Brush from '../../tools/Brush';
+import Rect from '../../tools/Rect';
 import styles from './toolsbar.module.scss'
 
 const ToolsBar = () => {
   return (
     <div className={styles.toolsbar}>
-      <button className={styles.brush} title="brush"/>
+      <button className={styles.brush} title="brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}/>
       <button className={styles.eraser} title="eraser"/>
       <button className={styles.line} title="line"/>
       <button className={styles.circle} title="circle"/>
-      <button className={styles.rect} title="rectangle"/>
+      <button className={styles.rect} title="rectangle" onClick={()=> toolState.setTool(new Rect(canvasState.canvas))}/>
       <input type="color" className={styles.palitra} title="color"/>
       <button className={styles.undo} title="undo"/>
       <button className={styles.redo} title="redo"/>
