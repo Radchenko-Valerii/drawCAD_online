4297bef25e24251e6ed4acbb40c58db2c67ede61
Valerii <valeravivaz@gmail.com>
2021-12-29 21:56:55 +0200

    Change eraser logic
    - add extending Brush class

--- client/src/tools/Eraser.js
+++ client/src/tools/Eraser.js
@@ -1,32 +1,11 @@
-import Tool from "./Tool";
+import Brush from "./Brush";
 
-export default class Eraser extends Tool {
+export default class Eraser extends Brush {
   constructor(canvas){
     super(canvas);
     this.listen()
   }
-
-  listen(){
-    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
-    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
-    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
-  }
-
-  mouseUpHandler(event){
-    this.mouseDown = false;
-  }
-
-  mouseDownHandler(event){
-    this.mouseDown = true;
-    this.ctx.beginPath();
-    this.ctx.moveTo(event.pageX - event.target.offsetLeft, event.pageY - event.target.offsetTop)
-  }
-
-  mouseMoveHandler(event){
-    if(this.mouseDown){
-      this.draw(event.pageX - event.target.offsetLeft, event.pageY - event.target.offsetTop)
-    }
-  }
+  
 
   draw(x, y){
     this.ctx.lineTo(x, y);
