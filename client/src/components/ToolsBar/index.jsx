682d87b51cbdd3b1d715f4d318887d5734f5a9ea
Valerii <valeravivaz@gmail.com>
2021-12-29 15:52:34 +0200

    Add change color function

--- client/src/components/ToolsBar/index.jsx
+++ client/src/components/ToolsBar/index.jsx
@@ -9,6 +9,12 @@ import Rect from '../../tools/Rect';
 import styles from './toolsbar.module.scss'
 
 const ToolsBar = () => {
+
+  const changeColor = (e) => {
+    // toolState.setStrokeColor(e.target.value);
+    toolState.setFillColor(e.target.value)
+  }
+
   return (
     <div className={styles.toolsbar}>
       <button className={styles.brush} title="brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}/>
@@ -16,7 +22,7 @@ const ToolsBar = () => {
       <button className={styles.line} title="line" onClick={()=> toolState.setTool(new Line(canvasState.canvas))}/>
       <button className={styles.circle} title="circle" onClick={()=> toolState.setTool(new Circle(canvasState.canvas))}/>
       <button className={styles.rect} title="rectangle" onClick={()=> toolState.setTool(new Rect(canvasState.canvas))}/>
-      <input type="color" className={styles.palitra} title="color"/>
+      <input onChange={e=>changeColor(e)} type="color" className={styles.palitra} title="Fill color"/>
       <button className={styles.undo} title="undo"/>
       <button className={styles.redo} title="redo"/>
       <button className={styles.save} title="save"/>
