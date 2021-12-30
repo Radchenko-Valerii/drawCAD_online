4cc1892cadcd0b700730eafa3ba8983cf69f4275
Valerii <valeravivaz@gmail.com>
2021-12-30 15:33:57 +0200

    Add setSocket & setSessionId

--- client/src/store/canvasState.js
+++ client/src/store/canvasState.js
@@ -2,12 +2,22 @@ import { makeAutoObservable } from "mobx";
 
 class CanvasState {
   canvas = null;
+  socket = null;
+  sessionId = null;
   undoList = [];
   redoList = [];
   username= "";
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  setSocket(socket){
+    this.socket = socket;
+  }
+
+  setSessionId(id){
+    this.sessionId = id;
   }
 
   setUsername(name){
