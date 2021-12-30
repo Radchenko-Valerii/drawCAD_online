d39c4b0124edda5419f7a51250405bde14054c84
Valerii <valeravivaz@gmail.com>
2021-12-30 02:52:22 +0200

    Add BrowserRouter & Redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 import Canvas from './components/Canvas';
 import ToolsBar from './components/ToolsBar';
 import ToolSettingsBar from './components/ToolSettingsBar';
-import "./styles/app.scss"
+import "./styles/app.scss";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="app">
-      <ToolsBar/>
-      <ToolSettingsBar/>
-      <Canvas/>
+      <Switch>
+        <Route path="/:id">
+        <ToolsBar/>
+        <ToolSettingsBar/>
+        <Canvas/>
+        </Route>
+        <Redirect to={`f${(+new Date).toString(16)}`} />
+      </Switch>
+
+      
     </div>
+    </BrowserRouter>
   );
 }
 
