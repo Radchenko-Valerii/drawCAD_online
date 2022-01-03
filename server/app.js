b475c52b56340c82eb84942c7c338070fd4167f6
Valerii <valeravivaz@gmail.com>
2022-01-03 23:53:44 +0200

    Add logic to post & get canvas image to client's

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
-
+const cors = require('cors')
 const PORT = process.env.PORT || 5000;
+const fs = require('fs');
+const path = require('path');
+
+app.use(cors());
+app.use(express.json())
 
 app.ws("/", (ws, req) => {
   ws.on("message", (msg) => {
@@ -18,6 +23,28 @@ app.ws("/", (ws, req) => {
     }
   });
 });
+
+app.post('/image', (req, res)=>{
+  try {
+    const data = req.body.img.replace('data:image/png;base64,', '');
+    fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64');
+    return res.status(200).json({message: "image post success"})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json('image post error')
+  }
+});
+
+app.get('/image', (req, res)=>{
+  try {
+    const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+    const data = 'data:image/png;base64,' + file.toString('base64');
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json('image get error');
+  }
+})
 
 app.listen(PORT, () => console.log(`server started sucsess PORT ${PORT}`));
 
